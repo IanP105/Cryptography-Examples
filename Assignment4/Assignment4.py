912bda62280b5f1c443ce23e35efde7667e3f15c
Ian Peña <107127545+IanP105@users.noreply.github.com>
2022-11-01 11:25:56 -0400

    Update Assignment4.py

--- Assignment4/Assignment4.py
+++ Assignment4/Assignment4.py
@@ -5,6 +5,10 @@ b = input("Enter the 2nd number of the GCD equation.")
 print("2nd input number is: " + b)
 b = int(b)
 
+if b == 0:
+    print("Denominator cannot be 0.")
+    system.exit()
+
 def gcd(a, b):
     while(b>0):
        a, b = b, a % b
@@ -12,3 +16,5 @@ def gcd(a, b):
 print()
 print("The GCD of the two input numbers is:")
 print(gcd(a,b))
+
+# I am not sure how to do this in a way that will show each step of the GCD equation, which is why a table is not printed.
